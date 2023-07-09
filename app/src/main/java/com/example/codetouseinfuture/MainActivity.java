69ccdbb447b8e020fa6685e690ce62776420b148
jc <jakubczarnecki@poczta.onet.pl>
2023-07-09 21:32:18 +0200

    Create second activity
    adding a second activity button to transition between activities

--- app/src/main/java/com/example/codetouseinfuture/MainActivity.java
+++ app/src/main/java/com/example/codetouseinfuture/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.codetouseinfuture;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btn_1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_1 = findViewById(R.id.btn_1);
+        btn_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
