02e94c2f7f1ef295142de31b4841b4c40706516e
jc <jakubczarnecki@poczta.onet.pl>
2023-07-11 00:10:53 +0200

    Update
    Update res and gradle

--- app/src/main/java/com/example/codetouseinfuture/MainActivity.java
+++ app/src/main/java/com/example/codetouseinfuture/MainActivity.java
@@ -7,18 +7,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.codetouseinfuture.Animation.AnimationActivity_1;
+
 public class MainActivity extends AppCompatActivity {
 Button btn_1;
+Button btn_2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btn_1 = findViewById(R.id.btn_1);
+        btn_2 = findViewById(R.id.btn_2);
         btn_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AnimationActivity_1.class);
                 startActivity(intent);
             }
         });
